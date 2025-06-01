312381c424eee841fadf2028e211371c9faedb05
Wilker94 <wilkerramello@gmail.com>
2025-06-01 09:02:52 -0300

    intro-grids

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
+
+#App.py = Arquivo principal que une tudo
+
+#carregando dependencias necessarias
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+#criando caminhos que app direciona
 @app.route('/')
 def home():
     return render_template('index.html')
 
 @app.route('/rich')
 def rich_page():
-    return render_template('rich.html')  # ou "return 'lalala'" se ainda não criou o rich.html
+    return render_template('rich.html') 
+
+@app.route('/landing')
+def landing_page():
+    return render_template('landing.html') 
 
 if __name__ == '__main__':
     app.run(debug=True)
